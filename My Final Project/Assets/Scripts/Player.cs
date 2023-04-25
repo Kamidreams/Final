c453286868bbceb92c1a82ffba773e88740259dd
Kamidreams <ahuante447@gmail.com>
2023-04-25 11:04:55 -0700

    Map Building / Player Script

--- My Final Project/Assets/Scripts/Player.cs
+++ My Final Project/Assets/Scripts/Player.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float speed = 10f;
+    public float rotationSpeed = 10f;
+
+    private Rigidbody _playerRb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+         _playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+         float horizontalInput = Input.GetAxis("Horizontal");
+        Vector3 forwardInput = transform.forward * Input.GetAxis("Vertical");
+
+        _playerRb.AddForce(forwardInput * speed, ForceMode.Acceleration);
+        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
     }
 }
