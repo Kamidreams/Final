b3cb9461182b00e430ad9c4c0645bcad140aec51
Kamidreams <ahuante447@gmail.com>
2023-05-02 11:05:52 -0700

    gdsg

---
+++ My Final Project/Assets/Scripts/Moving.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour
+{
+    public Transform[] PatrolPoints;
+    public float movespeed = 10;
+    public float StartWaitTime;
+
+    private int _currentPointIndex;
+    private float _waitTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = PatrolPoints[0].position;
+        transform.rotation = PatrolPoints[0].position;
+        _waitTime = StartWaitTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, PatrolPoints[_currentPointIndex].position, movespeed * Time.deltaTime);
+        if (transform.position == PatrolPoints[_currentPointIndex].position)
+        {
+            if (_waitTime <= 0)
+            {
+                if (_currentPointIndex + 1 < PatrolPoints.Length)
+                {
+                    _currentPointIndex++;
+                }
+                else
+                {
+                    _currentPointIndex = 0;
+                }
+                _waitTime = StartWaitTime;
+            }
+            else{
+                _waitTime -= Time.deltaTime;
+            }
+        }
+        //transform.Translate(Vector2.right * movespeed * Time.deltaTime);
+    }
+}
